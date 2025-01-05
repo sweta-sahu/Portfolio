619475fd3e7f948907d1076e7c8c5231a3c08171
Sweta <swetasahu2399@gmail.com>
2025-01-05 01:42:27 -0500

    deployment setup

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: isProd ? '/Porfolio/' : '',
-    basePath: isProd ? '/Porfolio' : '',
+    assetPrefix: isProd ? '/Portfolio/' : '',
+    basePath: isProd ? '/Portfolio' : '',
     output: 'export'
   };
   
