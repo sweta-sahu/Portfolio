9c6f542c8815b844ecbe7d13d11a1b4803747611
Sweta <swetasahu2399@gmail.com>
2025-01-05 13:08:46 -0500

    Updates

--- _next/static/chunks/app/skills/page-867308e0e2edb9db.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[501],{8702:(e,s,_)=>{Promise.resolve().then(_.bind(_,2800))}},e=>{var s=s=>e(e.s=s);e.O(0,[740,711,362,206,848,800,441,517,358],()=>s(8702)),_N_E=e.O()}]);
